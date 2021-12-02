426996100bce0d696f14fe0ea685c247a5b476aa
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-12-02 17:21:25 +0000

    Initial working copy

---
+++ api/v1/fmapp_types.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// FMAppSpec defines the desired state of FMApp
+type FMAppSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Format:=string
+	DeploymentName string `json:"deploymentName,omitempty"`
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Minimum=0
+	Replicas *int32 `json:"replicas"`
+}
+
+// FMAppStatus defines the observed state of FMApp
+type FMAppStatus struct { // INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	AvailableReplicas int32 `json:"replicas"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// FMApp is the Schema for the fmapps API
+type FMApp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FMAppSpec   `json:"spec,omitempty"`
+	Status FMAppStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// FMAppList contains a list of FMApp
+type FMAppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []FMApp `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&FMApp{}, &FMAppList{})
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module dev.quack.dev/guestbook
+
+go 1.16
+
+require (
+	github.com/go-logr/logr v0.4.0
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.15.0
+	istio.io/client-go v1.9.9 // indirect
+	k8s.io/api v0.22.1
+	k8s.io/apimachinery v0.22.1
+	k8s.io/client-go v0.22.1
+	sigs.k8s.io/controller-runtime v0.10.0
+)
